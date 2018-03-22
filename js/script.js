02ee7af250f57a73467536141a00851e6e784efe
Becky Segal <becsegal@gmail.com>
2018-03-22 10:48:12 -0400

    more promppts

--- js/script.js
+++ js/script.js
@@ -9,21 +9,24 @@ var prompts = [
   "What if you had half the time?",
   "Just stop. Try it.",
   "How have people solved similar problems?",
-  "Are you sure about that?", 
-  "*Nods thoughtfully*", 
+  "Are you sure about that?",
+  "*Nods thoughtfully*",
   "What is happening now?",
   "What should be happening?",
   "Describe the problem in 1 sentence",
   "What are some more options?",
   "Go on...",
   "A 20 minute walk could clear your head",
-  "That sounds difficult", 
+  "That sounds difficult",
   "I believe in you",
   "What does Google say on the subject?",
   "What's the use case?",
   "What are you leaning toward?",
   "Is this a priority?",
-  "Tackle something else for a while and let this one simmer"
+  "Tackle something else for a while and let this one simmer",
+  "What would the smartest person you know do?",
+  "What would your boss want you to do?",
+  "Are you hungry? Maybe get a snack"
 ];
 
 var app = new Vue({
